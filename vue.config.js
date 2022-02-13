2d1b990f967fff4c659763b7e8f2ebcde8ef501f
nina <nina@elnino.tech>
2022-02-13 22:39:25 +0100

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const bootstrapSassAbstractsImports = require("vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js");
 module.exports = {
+  lintOnSave: false,
   publicPath: "production" ? "/portfolio/" : "/",
   css: {
     loaderOptions: {
